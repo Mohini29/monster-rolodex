3d14bb1157f72ccade1df42ebbaf233d49142159
Mohini29 <agnihotrimohini29@gmail.com>
2020-08-31 23:57:54 +0530

    Updates

---
+++ precache-manifest.8f18dc8fce5f16f94577778278a35684.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9671fff51bb9c5cce99313daed4bf0e2",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "6ec748900244463978a4",
+    "url": "/monster-rolodex/static/css/main.73120491.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ec748900244463978a4",
+    "url": "/monster-rolodex/static/js/main.af74612e.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
